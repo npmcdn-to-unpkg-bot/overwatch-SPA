511ae6179514d10568b4b9ec07232dff6ac3657d
Alex Innes <alexinnes89@gmail.com>
2016-07-26 22:35:39 +1000

    App deployed and working - Next steps is working out next set of features

--- README.md
+++ README.md
@@ -1,10 +1,43 @@
 Overwatch LFG App
 
-Covering the requirements:
+Overwatch is a team based first person shooter game that allows users to choose from one of four categories (attack, defence, tank and support). The idea of the game is to create a balanced team between damage, healing, defence and tanks. The key to winning is a strong team. As this game is slowly becoming the next big E-Sports game I decided to make a single page app that allows users to enter their player details to post a request to form a team. The idea is that with the stats of the players and forming a balanced team you can have a better chance at winning.
 
-API Fetch: This will use the overwatch API to gather player data. Users can hover over a post to see that persons latest stats and info (kd ratio, icon, level etc)
-C.R.U.D: The user will fill out some fields user name (for api grab), if they're LFG or LFM (looking for members) and their prefered role they'd like to play as (out of Attack, Defense, Tank, Support). This will create content. User can then read the content as it gets added. The content will stay and continually be added in chronollogical order. At the moment it will be universally deletable but hopefully I can add OAuth so only content made by them can be deleted? hopefully?
+As a side note, this is a new game. The API i'm using is an official one as Blizzard hasn't released their API yet. It pulls current data but it's rather slow and requires to fetches (using promise.all) to get all the data I wanted. So appologies if it's a bit slow loading!
 
-Creating the app:
+The way the app works is it collects some input from the user: 
+	- Battle Net ID (to select the player you want data from)
+	- The platform that user plays on (between pc, playstation and xbox),
+	- The region you play on (between American servers and Europe servers)
+	- The Game mode (currently overwatch has quick-play and competitive modes)
+	- And any notes to tell players what you're looking for
 
-R
+I then collect this data through the delegate function on button click. It uses all of the data the user gives to create the url's needed for the fetch of the API.
+
+Since this is an unofficial API i'm using it's split up the data I want into two URL's. Since i'm using Firebase to push the results and therefore render the data into a row, I need the data to be pulled as one fetch and one push (as splitting it into two fetches and two pushes causes a double render with half the data in each). So to solve this I used promise.all() to fetch both of the URL's inside an array.
+
+Now that the data is collected and pushed, it triggers the .on function from Firebase. This triggers when anything has been added,removed,updated to firebase and will re render based on the action.
+
+The render works by calling the data that's stored in firebase inside a state.users array. Each render is given the key stored in firebase which allows me to edit and delete these specific rows that have been rendered.
+
+The Render contains:
+	- User icon
+	- User Name
+	- Level
+	- Games Won
+	- Time Played
+	- What mode you want to play
+	- Your notes made
+
+All of this data is stored in firebase. I chose this data to be easily presented in a table and hope it helps users make a decision on who to choose on their team.
+
+To delete the data/row that's rendered, I create a getKeyFromClosestElement() function that finds the closest key based on a delegate key click function. With that data I can then remove the specific data based on the firebase key found.
+
+As the update feature, I've added a checkbox to update when a user has found a group. This alters a simple bit of text "show" or "hide" which is stored inside of a class. It overlays a big green tick over their icon to indicate they've found a group.
+
+As an added feature i've added a time since posted function. At the time of rendering i capture the current date using new Date().getTime() / 1000 (second part to turn it into epoch number instead of date format which is needed for the function). The function then compares the current time to the time that the post was created. This is then rendered in either: Seconds, Minutes, Hours or Years since the post was created.
+
+//// What's Next
+
+As this is a work in progress, the next steps i'd like to do is to create a login function for two reasons. This will allow only that user to post their own characters data. This will also limit which posts they can delete. I'd also like to add more functionallity to form a group of people inside the SPA by selecting posts to see your formed team of 6.
+
+I'd also ideally like to create a chat function between users. This way users can talk to each other and communicate how to join up in game.
--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
 (function() {
 
-	// Initialize Firebase
-	let firebaseAppName = "alexinnes-firebase";
-	let firebaseKey = "AIzaSyALk4SqOf183IoFlQohoEucdkLihFkabpU";
-
+	// Initialize Firebase - Enter your firebase key here...
+	let firebaseAppName = "Firebase-Name-Here";
+	let firebaseKey = "FirebaseKey-Here";
 	var config = {
 		apiKey: firebaseKey,
 		authDomain: `${firebaseAppName}.firebaseapp.com`,
@@ -12,26 +11,24 @@
 	};
 	firebase.initializeApp(config);
 
-	firebase.database().ref('tasks/').on('value', function(snapshot) {
+	firebase.database().ref('users/').on('value', function(snapshot) {
 		// Pull the list value from firebase
-		state.list = snapshot.val();
-		renderArticle(state, container)
+		state.users = snapshot.val();
+		renderUsers(state, container)
 	});
-
+	// End Firebase initialize
+	
 	var state = {
-			overwatchApi : "",
-			overwatchPlayerApi: "",
-			gamesPlayed: "",
-			gamesWon: "",
-			timePlayed: "",
-			soloKills: "",
-			playerIcon: "",
-			playerLevel: ""
+		overwatchApi : "",
+		overwatchPlayerApi: "",
+		postTime: ""
 	}
 
 	var container = document.querySelector('.lfg-container')
+	var getDate = new Date().getTime() / 1000
+	state.postTime = getDate
 
-
+	// Collect all data to perform the render
 	delegate('.dark-background','click','.yellow-btn', (event) => {
 		event.preventDefault()
 		var playertagValue = document.querySelector('#player-tag').value
@@ -39,59 +36,95 @@
 		var regionValue = document.querySelector('#region-value').value
 		var gameModeValue = document.querySelector('#gamemode-value').value
 		var container = document.querySelector('.lfg-container')
+		state.gameMode = gameModeValue
 		state.notesValue = document.querySelector('#notes-value').value
 		state.overwatchApi =  "https://api.lootbox.eu/" + platformValue + "/" + regionValue + "/" + playertagValue + "/" + gameModeValue + "/allHeroes/"
 		state.overwatchPlayerApi = "https://api.lootbox.eu/" + platformValue + "/" + regionValue + "/" + playertagValue + "/profile"
 		overwatchFetch()
 	})
 
+	// Remove specific post
+	delegate('.lfg-container', 'click', '.remove', (event) => {
+		let key = getKeyFromClosestElement(event.delegateTarget);
+		// Remove that particular key
+		firebase.database().ref(`users/${key}/`).remove();
+	});
+
+	// Update if you've found a group
+	delegate('.lfg-container', 'click', '.update', (event) => {
+		let key = getKeyFromClosestElement(event.delegateTarget);
+		if (!state.users[key].done) {
+			firebase.database().ref(`users/${key}/`).update({
+				done: event.delegateTarget.checked,
+				foundGroup: "show"
+			});
+		} else {
+			firebase.database().ref(`users/${key}/`).update({
+				done: false,
+				foundGroup: "hide"
+			});
+		}
+	});
+
+	// Unofficial Overwatch  API fetch
 	function overwatchFetch(){
 		renderLoading(container)
-		fetch(state.overwatchApi)
-		.then((response) => {
-			return response.json()
-		})
-		.then((result) => {
-			state.gamesPlayed = result.GamesPlayed
-			state.gamesWon = result.GamesWon
-			state.timePlayed = result.TimePlayed
-			state.soloKills = result.SoloKills
-		})
-		.catch((err)=>{
-			alert("The Overwatch API returned this error:" + err)
-		})
+		var f1 = fetch(state.overwatchApi).then((response)=>{return response.json()})
+		var f2 = fetch(state.overwatchPlayerApi).then((response)=>{return response.json()})
 
-		fetch(state.overwatchPlayerApi)
-		.then((response) => {
-			return response.json()
-		})
-		.then((result) => {
-			state.playerIcon = result.data.avatar
-			state.playerTag = result.data.username
-			state.playerLevel = result.data.level
-			renderArticle(state, container)
+		Promise.all([f1,f2])
+		.then(ress => {
+			firebase.database().ref('users/').push({
+				gameMode: state.gameMode,
+				gamesPlayed: ress[0].GamesPlayed,
+				gamesWon: ress[0].GamesWon,
+				timePlayed: ress[0].TimePlayed,
+				soloKills: ress[0].SoloKills,
+				playerIcon: ress[1].data.avatar,
+				playerLevel: ress[1].data.level,
+				playerTag: ress[1].data.username,
+				notesValue: state.notesValue,
+				postTime: state.postTime,
+				done: false,
+				foundGroup: "hide"
+			});
+
+			// Reset the input value ready for a new item
+			document.querySelector('#player-tag').value = '';
+			document.querySelector('#notes-value').value = '';
 		})
 		.catch((err)=>{
 			alert("The OverwatchPlayer API returned this error:" + err)
 		})
 	}
 	
-	function renderArticle(state, container) {
-		container.innerHTML = Object.keys(state.list).map((key) => {
-			return	`<table class="col-xs-12 margin-top lfg-row">
+	// Render Looking For Group Post
+	function renderUsers(state, container) {
+		container.innerHTML = 
+		Object.keys(state.users).map((key) => {
+			return `<table class="col-xs-12 margin-top lfg-row">
 			<tr class="well" data-id="${key}">
-				<td><img class="player-icon" src="${state.list[key].playerIcon}"></td>
+				<td class="avatar-container">
+					<div class="found-group ${state.users[key].foundGroup}">
+						<i class="fa fa-check"></i>
+					</div>
+					<img class="player-icon" src="${state.users[key].playerIcon}">
+				</td>
 				<td class="player-details">
-					<h4>${state.list[key].playerTag}</h4>
-					<h5 class="player-level"><i class="fa fa-star"> </i> <strong>Level:</strong> ${state.list[key].playerLevel}</h5>
+					<h4>${state.users[key].playerTag}</h4>
+					<h5 class="player-level"><i class="fa fa-star"> </i> <strong>Level:</strong> ${state.users[key].playerLevel}</h5>
+					<p><strong>Games Played:</strong> ${state.users[key].gamesPlayed}</p>
+					<p><strong>Games Won:</strong> ${state.users[key].gamesWon}</p>
+					<p><strong>Time Played:</strong> ${state.users[key].timePlayed}</p>
 				</td>
 				<td class="looking-for">
-					<h4>Looking for Competitive</h4>
-					<p>${state.list[key].notesValue}</p>
+					<h4>Looking for: ${state.users[key].gameMode}</h4>
+					<p>${state.users[key].notesValue}</p>
 				</td>
 				<td class="time-posted">
 					<span class="remove pull-right"><i class="fa fa-times"></i></span>
-					<span class="time-post">3 minutes ago</span>
+					<span class="time-post">${timePosted(state.users[key].postTime)}</span>
+					<label class="checkbox-label">I found a group! <input class="update" type="checkbox" ${state.users[key].done ? "checked" : ""} /></label>
 				</td>
 			</tr>
 		</table>
@@ -99,20 +132,64 @@
 		}).join('');
 	}
 
+	// Loading function during fetch (This api is unofficial and is rather slow)
 	function renderLoading(into) {
-		container.innerHTML = `<div id="pop-up" class="loader"></div>`
+		container.innerHTML = 
+		`<div class="second-wrapper">
+			<div class="circle-loader">
+				<div class="circle circle_four"></div>
+				<div class="circle circle_three"></div>
+				<div class="circle circle_two"></div>
+				<div class="circle circle_one"></div>
+			</div>
+		</div>`
 	}
 
+	// Find closest key function
 	function getKeyFromClosestElement(element) {
-		// Search for the closest parent that has an attribute `data-id`
 		let closestItemWithId = closest(event.delegateTarget, '[data-id]')
 		if (!closestItemWithId) {
 			throw new Error('Unable to find element with expected data key');
 		}
-		// Extract and return that attribute
 		return closestItemWithId.getAttribute('data-id');
 	}
 
+	// Time since posted function
+	function timePosted(epoch) {
+		var secs = ((new Date()).getTime() / 1000) - epoch;
+		Math.floor(secs);
+		var minutes = secs / 60;
+		secs = Math.floor(secs % 60);
+		if (minutes < 1) {
+			return secs + (secs > 1 ? ' seconds ago' : ' second ago');
+		}
+		var hours = minutes / 60;
+		minutes = Math.floor(minutes % 60);
+		if (hours < 1) {
+			return minutes + (minutes > 1 ? ' minutes ago' : ' minute ago');
+		}
+		var days = hours / 24;
+		hours = Math.floor(hours % 24);
+		if (days < 1) {
+			return hours + (hours > 1 ? ' hours ago' : ' hour ago');
+		}
+		var weeks = days / 7;
+		days = Math.floor(days % 7);
+		if (weeks < 1) {
+			return days + (days > 1 ? ' days ago' : ' day ago');
+		}
+		var months = weeks / 4.35;
+		weeks = Math.floor(weeks % 4.35);
+		if (months < 1) {
+			return weeks + (weeks > 1 ? ' weeks ago' : ' week ago');
+		}
+		var years = months / 12;
+		months = Math.floor(months % 12);
+		if (years < 1) {
+			return months + (months > 1 ? ' months ago' : ' month ago');
+		}
+		years = Math.floor(years);
+		return years + (years > 1 ? ' years ago' : ' years ago');
+	}
+
 })()
-
-
